88e135e07863ba6527aede370e1d651c9a92f4c3
Amir Eldor <amir@eldor.dev>
2024-11-15 03:46:46 +0200

    Revert to original template files and start migrating tomer changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
-(function ($) {
+
+  (function ($) {
   
   "use strict";
 
@@ -9,51 +10,35 @@
     $('.navbar-nav .nav-link').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
-  
-   
 
     // NEWS IMAGE RESIZE
-    function NewsImageResize() {
-        // בדוק אם האלמנט עם class 'navbar' קיים
-        if ($(".navbar").length) {
-            $(".navbar").scrollspy({ offset: -76 });
-        } else {
-            console.warn('Navbar element not found!');
-        }
+    function NewsImageResize(){
+      $(".navbar").scrollspy({ offset: -76 });
+      
+      var LargeImage = $('.large-news-image').height();
 
-        // בדוק אם האלמנט עם class 'large-news-image' קיים
-        var largeImageElement = $('.large-news-image');
-        if (largeImageElement.length) {
-            var LargeImage = largeImageElement.height();
-            var MinusHeight = LargeImage - 6;
+      var MinusHeight = LargeImage - 6;
 
-            // בדוק אם האלמנט עם class 'news-two-column' קיים
-            var newsTwoColumnElement = $('.news-two-column');
-            if (newsTwoColumnElement.length) {
-                newsTwoColumnElement.css({'height': (MinusHeight - LargeImage / 2) + 'px'});
-            } else {
-                console.warn('News two-column element not found!');
-            }
-        } 
+      $('.news-two-column').css({'height' : (MinusHeight - LargeImage / 2) + 'px'});
     }
 
-    // הפעלת הפונקציה על resize ועל ready
     $(window).on("resize", NewsImageResize);
     $(document).on("ready", NewsImageResize);
 
-    // אנימציית גלילה
     $('a[href*="#"]').click(function (event) {
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top - 66
-                }, 400);
-            }
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 66
+          }, 1000);
         }
+      }
     });
     
-})(window.jQuery);
+  })(window.jQuery);
+
+  
